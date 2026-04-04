d14b7c0ca4dba989372b6673e260b4d1a9d8ceb5
yogita410 <yogita@goodeducator.com>
2026-04-04 15:45:36 +0530

    Initial Commit

---
+++ jprac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jprac
+//
+//  Created by Yogita on 04/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ jpracTests/jpracTests.swift
@@ -0,0 +1,17 @@
+//
+//  jpracTests.swift
+//  jpracTests
+//
+//  Created by Yogita on 04/04/26.
+//
+
+import Testing
+@testable import jprac
+
+struct jpracTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
