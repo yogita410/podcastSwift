da79a4200ee660f75898ac189791483f0b02cc1e
yogita410 <yogita@goodeducator.com>
2026-04-05 23:33:24 +0530

    Wire storyboard and clean up default ViewController
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- jprac/ViewController.swift
+++ jprac/ViewController.swift
@@ -1,19 +1,1 @@
-//
-//  ViewController.swift
-//  jprac
-//
-//  Created by Yogita on 04/04/26.
-//
-
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
